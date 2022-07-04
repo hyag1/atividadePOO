2921b141ef10208c0e09ec639dd5c53e013719d8
hyag1 <hyagoluca1@gmail.com>
2022-07-03 21:43:54 -0300

    Funcoes aluno historico e matricula

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import sqlite3
-from flask import Blueprint,Flask,render_template,request,flash, redirect, url_for
+from flask import Blueprint,Flask,render_template,request,flash, redirect, url_for,Response
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import insert
 from config import app_config,app_active
-from model import Usuario
+from model import *
 
 
 config=app_config[app_active]
@@ -16,6 +17,8 @@ def create_app(config_name):
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
     db=SQLAlchemy(app)
     db.init_app(app)
+    conn = sqlite3.connect('database/minisigaa.db', check_same_thread=False)
+    cur = conn.cursor()
 
     @app.route("/")
     def index():
@@ -27,8 +30,7 @@ def create_app(config_name):
     #         return redirect(url_for('index'))
     #     else:
     #         return render_template("perfil.html",data=data)
-    conn = sqlite3.connect('database/minisigaa.db',check_same_thread=False)
-    cur = conn.cursor()
+
 
     @app.route('/login')
     def login():
@@ -42,13 +44,12 @@ def create_app(config_name):
             user: Usuario = Usuario.query.filter_by(nome=login).first()
             if user:
                 if senha == user.senha:
-                    print(user);
                     if user.role == 1:
-                        return adm(user.id)
+                        return redirect(url_for('adm',id = user.id))
                     elif user.role == 2:
-                        return aluno(user.id)
+                        return redirect(url_for('aluno', id=user.id))
                     else:
-                        return professor(user.id)
+                        return redirect(url_for('professor', id=user.id))
                 else:
                     flash('Senha Incorreta tenta de novo ai!')
                     return redirect(url_for('login'))
@@ -56,24 +57,84 @@ def create_app(config_name):
                 flash('Deu certo não tente ai de novo please!')
                 return redirect(url_for('login_post'))
 
-    @app.route('/adm/<id>')
+    @app.route('/login/adm/<id>')
     def adm(id):
         user: Usuario = Usuario.query.filter_by(id=id).first()
         return render_template("adm.html",usu=user)
 
     @app.route('/lista_alunos')
-    def modal():
+    def modal_lista_alunos():
         cur.execute("SELECT * FROM usuario where role=2")
         alunos = cur.fetchall()
-        return render_template("modalListar.html",alu=alunos);
+        return render_template("modalListarAlunos.html",alu=alunos)
+
+    @app.route('/login/aluno/<id>/listardisciplinas')
+    def modal_lista_disciplinas(id):
+        cur.execute("SELECT * FROM disciplinas")
+        disciplina_array = cur.fetchall()
+        return render_template("modalListarDisciplinas.html", disciplinas=disciplina_array,val =id)
+
+    @app.route('/login/aluno/<id>/listardisciplinas',methods = ['POST'])
+    def matricular(id):
+        ide = request.form['id']
+        disci = request.form['disci']
+        disciplina: Disciplinas = Disciplinas.query.filter_by(id=disci).first()
+        cur.execute("SELECT * FROM matricula")
+        matriculas = cur.fetchall()
+        if len(matriculas)!=0:
+            for matricula in matriculas:
+                if str(matricula[1]) == ide and str(matricula[2]) == disci:
+                    flash('Você já está cadastrado nesta disciplina!')
+                    return redirect(url_for('aluno',id = ide))
+                else:
+                    pass
+            cur.execute("INSERT INTO matricula (aluno_id, disciplina_id) VALUES (?, ?)", [ide, disci])
+            conn.commit()
+            flash('Ótimo você se matriculou na disciplina!')
+            return redirect(url_for('aluno',id = ide))
+        else:
+            cur.execute("INSERT INTO matricula (aluno_id, disciplina_id) VALUES (?, ?)",[ide,disci])
+            conn.commit()
+            flash('Ótimo você se matriculou na disciplina!')
+            return redirect(url_for('aluno',id = ide))
+
+    @app.route('/login/aluno/<id>/matriculado')
+    def minhas_disciplinas(id):
+        historico=[]
+        disciplinas=[]
+        cur.execute("SELECT * FROM matricula where aluno_id = ?",[id])
+        matriculas = cur.fetchall()
+        for matricula in matriculas:
+            cur.execute("SELECT * FROM disciplinas where id = ?", [matricula[1]])
+            disciplinas_name = cur.fetchall()
+            disciplinas.append(disciplinas_name)
+        for matricula in matriculas:
+            for disciplina in disciplinas_name:
+                print(disciplina)
+                print(matricula)
+                historico.append([disciplina[2],disciplina[3],matricula[3],matricula[4],matricula[5]])
+        print(historico)
+        return render_template("historico.html",hist = historico)
 
 
-    @app.route('/aluno/<id>')
+
+    # @app.route('/lista_disciplinas/<int:ide>', methods=['POST'])
+    # def excluir_disciplina(ide):
+    #     disciplina_to_del = Disciplinas.query.filter_by(id = ide).frist()
+    #     if request.method == 'POST':
+    #         if disciplina_to_del:
+    #             db.session.delete(disciplina_to_del)
+    #             db.session.commit()
+    #         else:
+    #             flash('Disciplina não encontrada')
+    #         return flash('Disciplina '+disciplina_to_del[3]+' excluida!');
+
+    @app.route('/login/aluno/<id>')
     def aluno(id):
         user: Usuario = Usuario.query.filter_by(id=id).first()
-        return render_template("aluno.html",usu=user)
+        return render_template("aluno.html",usu=user, ident = id)
 
-    @app.route('/professor/<id>')
+    @app.route('/login/professor/<id>')
     def professor(id):
         user: Usuario = Usuario.query.filter_by(id=id).first()
         return render_template("professor.html",usu=user)
--- model.py
+++ model.py
@@ -3,13 +3,13 @@ from config import app_config,app_active
 from flask import Flask
 from flask_script import Manager
 from flask_migrate import Migrate,MigrateCommand
-
+from datetime import datetime
 config=app_config[app_active]
 
 if __name__=='__main__':
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = config.SQLALCHEMY_DATABASE_URI
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
     db=SQLAlchemy(app)
     migrate = Migrate(app,db)
@@ -23,33 +23,37 @@ class Role(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     nome = db.Column(db.String(40),unique=True,nullable=False)
 
-class Disciplinas(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    descricao = db.Column(db.String(40), unique=True, nullable=False)
-    max = db.Column(db.Integer, nullable=True)
-    n1 = db.Column(db.Integer, nullable=True)
-    n2 = db.Column(db.Integer, nullable=True)
-    n3 = db.Column(db.Integer, nullable=True)
-
-    def __init__(self,descricao,max,n1,n2,n3):
-        self.descricao=descricao
-        self.max=max
-        self.n1=n1
-        self.n2=n2
-        self.n3=n3
-
 class Usuario(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     nome = db.Column(db.String(40),unique=True,nullable=False)
     senha = db.Column(db.String(40),nullable=False)
-    disciplina_id = db.Column(db.Integer,db.ForeignKey(Disciplinas.id),nullable=True)
+    ultimo_login = db.Column(db.DateTime, nullable=False)
     role = db.Column(db.Integer,db.ForeignKey(Role.id),nullable=False)
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args, **kwargs)
+        self.ultimo_login=datetime.now()
 
-    def __init__(self,nome,senha,disciplina_id,role):
-        self.nome=nome
-        self.senha=senha
-        self.disciplina_id=disciplina_id
-        self.role=role
+class Disciplinas(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    descricao = db.Column(db.String(40), unique=True, nullable=False)
+    codigo = db.Column(db.Integer, unique=True, nullable=False)
+    max = db.Column(db.Integer, nullable=False)
+    professor_id = db.Column(db.Integer,db.ForeignKey(Usuario.id),nullable=True)
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args, **kwargs)
 
+class Matricula(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    aluno_id = db.Column(db.Integer,db.ForeignKey(Usuario.id),nullable=False)
+    disciplina_id = db.Column(db.Integer,db.ForeignKey(Disciplinas.id),nullable=False)
+    n1 = db.Column(db.Integer, nullable=True)
+    n2 = db.Column(db.Integer, nullable=True)
+    n3 = db.Column(db.Integer, nullable=True)
+    def __init__(self,aluno_id,disciplina_id,n1,n2,n3):
+        self.aluno_id =aluno_id
+        self.disciplina_id = disciplina_id
+        self.n1 = n1
+        self.n2 = n2
+        self.n3 = n3
 if __name__=='__main__':
     menager.run()
